da9b70b82a81536fa45f2420102880f09a5e23b6
nikhila1234 <36685049+nikhila1234@users.noreply.github.com>
2021-04-25 17:33:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Mlchai" && b=="26SN01")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
